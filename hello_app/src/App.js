a2b90e5f613c99014fb5d18d0b79bad505d2e092
Mrkingggg <1529716150@qq.com>
2024-03-25 13:32:05 -0400

    hello_app_simple

---
+++ hello_app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Hello App!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
