b003626cd4e4ff3636d175722c56071bd1d095ee
Mrkingggg <1529716150@qq.com>
2024-03-25 22:43:24 -0400

    u

--- frontend/src/AddUser.js
+++ frontend/src/AddUser.js
@@ -43,8 +43,18 @@ const AddUser = () => {
       <h2>Add User</h2>
       <form onSubmit={handleSubmit}>
         <input type="text" name="username" value={formData.username} onChange={handleChange} placeholder="Username" required />
+        <input type="text" name="password" value={formData.password} onChange={handleChange} placeholder="password" required />
         <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Email" required />
-        {/* 添加其他输入字段 */}
+        <p>dob:</p>
+        <input type="date" name="dob" value={formData.dob} onChange={handleChange} placeholder="Dob" required />
+        <input type="text" name="fullname" value={formData.fullname} onChange={handleChange} placeholder="fullname" required />
+        <select name="gender" value={formData.gender} onChange={handleChange} required>
+          <option value="">Select Gender</option>
+          <option value="female">Female</option>
+          <option value="male">Male</option>
+          <option value="other">Other</option>
+        </select>
+
         <button type="submit">Submit</button>
       </form>
     </div>
